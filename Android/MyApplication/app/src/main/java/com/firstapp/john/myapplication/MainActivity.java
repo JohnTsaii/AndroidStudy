75fbc20861d0b85ce4dd9bac3c236189fbf0448f
John Tsai <caijingzhuang@gmail.com>
2016-05-04 09:41:33 +0800

    add new feature

--- Android/MyApplication/app/src/main/java/com/firstapp/john/myapplication/MainActivity.java
+++ Android/MyApplication/app/src/main/java/com/firstapp/john/myapplication/MainActivity.java
@@ -1,14 +1,30 @@
 package com.firstapp.john.myapplication;
 
+import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import java.lang.String;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.johntsaii.myfisrstapp.extramessage";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
